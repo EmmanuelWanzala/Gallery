366958518a3bed8c2454cdef789241a0b2d6f8e3
EmmanuelWanzala <wanzalaemmanuel28@gmail.com>
2020-10-14 21:57:28 +0300

    add settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -135,7 +135,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
-# Simplified static file serving.
+# Simplified static  file serving.
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
